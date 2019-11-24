91f3a814cafe6001c19560838a8c6a7f235a75fc
pabloamado <54250734+pabloamado@users.noreply.github.com>
2019-11-24 18:06:26 -0300

    Add files via upload

---
+++ ejer humedad/main.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LONG_FRASE 80
+
+typedef struct {
+    int hora;
+    float humMin;
+    float humMax;
+    float humProm;
+    //float humAcum;
+    //float cant;
+} st_Medicion;
+
+void obtenerHoraHumedad(char *linea, int *hora, float *humedad) {
+    char marca = ';';
+    int cont = 0;
+    char *horario=(char*)malloc(sizeof(char)*2);
+    char *token;
+
+    token = strtok(linea, &marca);
+
+    while (token != NULL) {
+        cont++;                  
+        if (cont == 3) {
+            strncpy(horario, token, 2);
+            *hora = atoi(horario);
+        }
+        if (cont == 5) {
+            *humedad = atof(token);
+        }
+
+        token = strtok(NULL, &marca);
+
+    }
+
+}
+
+void inicializarBinario(FILE * binario) {
+    st_Medicion medicion;
+    medicion.hora = -1;
+    medicion.humMax = -1;
+    medicion.humMin = 100;
+    medicion.humProm = 0;
+   // medicion.humAcum = 0;
+   // medicion.cant = 0;
+    for (int i = 0; i < 24; i++) {
+        fwrite(&medicion, sizeof (st_Medicion), 1, binario);
+    }
+    fseek(binario,0,SEEK_SET);
+}
+
+void calcularHumedadProm(FILE * binario) {
+    fseek(binario, 0, SEEK_SET);
+    st_Medicion medicion;
+    for (int i = 0; i < 24; i++) {
+        fread(&medicion, sizeof (st_Medicion), 1, binario);
+        medicion.humProm = (medicion.humMax - medicion.humMin)/60;
+        fseek(binario, sizeof (st_Medicion) * i, SEEK_SET);
+        fwrite(&medicion, sizeof (st_Medicion), 1, binario);
+    }
+}
+
+void actualizarMedicion(st_Medicion *medicion, int hora, float humedad) {
+    if (medicion->hora == -1) {                      
+        medicion->hora = hora;                       
+    }
+    if (humedad > medicion->humMax) {
+        medicion->humMax = humedad;
+    }
+    if (humedad < medicion->humMin) {
+        medicion->humMin = humedad;
+    }
+   // medicion->cant++;
+   // medicion->humAcum += humedad;
+}
+ 
+void imprimirBinario(FILE * binario){
+    st_Medicion medFinal;
+    binario=fopen("binario.bin", "rb");
+    if(binario==NULL){
+        printf("Error al intentar abrir de nuevo el archivo binario");     
+    }
+    for (int i = 0; i < 24; i++) {
+        fread(&medFinal,sizeof(st_Medicion),1,binario);
+        
+        printf("hora: %d \n",medFinal.hora);
+        printf("humedad maxima %4.2f \n",medFinal.humMax);
+        printf("humedad min;%4.2f \n", medFinal.humMin );
+        printf("humedad promen dicha hora: %4.2f \n", medFinal.humProm);
+        
+    }
+}
+
+
+int main(int argc, char** argv) {
+    int hora = 0;
+    float humedad = 0;
+    char * linea=(char*)malloc(sizeof(char)*LONG_FRASE);
+    st_Medicion med;
+
+    memset(linea, '\0', LONG_FRASE);
+    FILE *archivoBin = NULL;
+    FILE *archivoTxt = NULL;
+    archivoBin = fopen("binario.bin", "w+b"); // ABRO CON EL + PORQUE SINO TIRA ERROR
+    archivoTxt = fopen("mediciones.txt", "r+");
+   
+    
+    inicializarBinario(archivoBin);
+
+    fgets(linea, 50, archivoTxt);
+    while (!feof(archivoTxt)) {
+
+        obtenerHoraHumedad(linea, &hora, &humedad);
+
+        fseek(archivoBin, sizeof (st_Medicion) * hora, SEEK_SET);
+
+        fread(&med, sizeof (st_Medicion), 1, archivoBin);
+
+        actualizarMedicion(&med, hora, humedad);
+
+        fseek(archivoBin, sizeof (st_Medicion) * hora, SEEK_SET);
+
+        fwrite(&med, sizeof (st_Medicion), 1, archivoBin);
+
+        fgets(linea, 50, archivoTxt);
+    } //TERMINA DE LEER EL ARCHIVO DE TEXTO.
+
+    //AHORA RECORRO EL ARCHIVO BIN PARA ACTUALIZAR EL PROMEDIO DE HUMEDADES;
+    calcularHumedadProm(archivoBin);
+    
+    fclose(archivoTxt);
+    fclose(archivoBin);
+    
+    imprimirBinario(archivoBin);
+
+    fclose(archivoBin);
+
+
+
+    
+
+    return (EXIT_SUCCESS);
+
+}
+
